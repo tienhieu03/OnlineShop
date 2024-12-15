55c398fa036baf9a693c9d35d9fa7edad7c71b6d
Tien Hieu <122421124+tienhieu03@users.noreply.github.com>
2024-12-15 17:56:24 +0700

    update homepage

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+
+const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+
+    console.log(products)
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default LatestCollection
