53b59622dd27cbd8239b4e4bc06d72b50c68c244
Tien Hieu <122421124+tienhieu03@users.noreply.github.com>
2024-12-17 04:21:21 +0700

    update search, product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,17 +11,19 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
     <div className = 'px-4 sm:px-[5vw] md:px-[7vw] lq:px-[9vw]'>
       <Navbar />
+      <SearchBar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
-        <Route path='/product:productId' element={<Product/>} />
+        <Route path='/product/:productId' element={<Product/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/place-order' element={<PlaceOrder/>} />
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
+
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData,setProductData] = useState(false);
+
+  const fetchProductData = async () => {
+
+    products.map((item) =>{
+      if(item._id === productId){
+        setProductData(item);
+        console.log(item);
+        return null;
+      }
+    })
+
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  },[productId])
+
   return (
-    <div>Product</div>
+    <div>
+      
+    </div>
   )
 }
 
