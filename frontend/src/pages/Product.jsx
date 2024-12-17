38d25efb769293f9b1cdf365bede234762052868
Tien Hieu <122421124+tienhieu03@users.noreply.github.com>
2024-12-18 04:12:56 +0700

    add cart total and related products components; refactor Title component

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProduct from "../components/RelatedProduct";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
@@ -80,8 +81,49 @@ const Product = () => {
               ))}
             </div>
           </div>
+          <button
+            onClick={() => addToCart(productData._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+          >
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange within 7 days.</p>
+          </div>
         </div>
       </div>
+
+      {/* ------------ Description and Review Section ------------ */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            cum, alias tenetur aut rem modi iure iste doloremque dolores,
+            voluptas eos dignissimos corrupti, consequuntur nisi. Aspernatur
+            optio itaque odit nesciunt.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem cum
+            inventore, fugiat blanditiis modi neque voluptatum fuga doloremque,
+            quam quisquam voluptatibus asperiores perferendis, aut sed alias
+            sint tenetur ipsam laudantium.
+          </p>
+        </div>
+      </div>
+
+      {/* ------------ Similar Products ------------ */}
+
+      <RelatedProduct
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
